03e445fe77da39cad5d82502c22f8353469e0373
Bruno Sonnino <sonnino@revolution.com.br>
2019-08-15 08:20:45 -0400

    Comments

--- PrimeNumbers/GeneratePrimes.cs
+++ PrimeNumbers/GeneratePrimes.cs
@@ -3,22 +3,9 @@
 namespace PrimeNumbers
 {
 /**
-* This class Generates prime numbers up to a user specified
-* maximum. The algorithm used is the Sieve of Eratosthenes.
-* <p>
-* Eratosthenes of Cyrene, b. c. 276 BC, Cyrene, Libya --
-* d. c. 194, Alexandria. The first man to calculate the
-* circumference of the Earth. Also known for working on
-* calendars with leap years and ran the library at Alexandria.
-* <p>
-* The algorithm is quite simple. Given an array of integers
-* starting at 2. Cross out all multiples of 2. Find the next
-* uncrossed integer, and cross out all of its multiples.
-* Repeat untilyou have passed the square root of the maximum
-* value.
-*
-* @author Alphonse
-* @version 13 Feb 2002 atp
+    * This class Generates prime numbers up to a user specified
+    * maximum. The algorithm used is the Sieve of Eratosthenes.
+    *  https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes   
 */
     public class GeneratePrimes
     {
